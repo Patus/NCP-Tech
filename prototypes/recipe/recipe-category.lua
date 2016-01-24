5a6b819e13f346439b4fe36b721b150d518241fe
Patus <patsi91@hotmail.com>
2016-01-24 16:52:17 +0200

    recipe-categorys

---
+++ prototypes/recipe/recipe-category.lua
@@ -0,0 +1,65 @@
+data:extend(
+{
+  {
+    type = "recipe-category",
+    name = "Grinder"
+  },
+  {
+    type = "recipe-category",
+    name = "Ore_washing"
+  },
+  {
+    type = "recipe-category",
+    name = "Chemical_plant"
+  },
+  {
+    type = "recipe-category",
+    name = "Electrolyzer"
+  },
+  {
+    type = "recipe-category",
+    name = "Chemical_furnace"
+  },
+  {
+    type = "recipe-category",
+    name = "Chemical_bath"
+  },
+  {
+    type = "recipe-category",
+    name = "Blast_furnace"
+  },
+  {
+    type = "recipe-category",
+    name = "Nuclear_fuel_processing_plant"
+  },
+  {
+    type = "recipe-category",
+    name = "Rolling_machine"
+  },
+  {
+    type = "recipe-category",
+    name = "Mixer"
+  },
+  {
+    type = "recipe-category",
+    name = "Shifter"
+  },
+  {
+    type = "recipe-category",
+    name = "Silicon_crucible"
+  },
+  {
+    type = "recipe-category",
+    name = "Cracking_plant"
+  },
+  {
+    type = "recipe-category",
+    name = "Distillation"
+  },
+  {
+    type = "recipe-category",
+    name = "Dry_distillation"
+  },
+  
+  
+})
