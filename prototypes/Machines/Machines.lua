cc41e05f58306cc1e6fd013714ebcdea8ef48cf2
Patus <patsi91@hotmail.com>
2016-03-09 10:01:17 +0200

    Electric_flash_furnace

--- prototypes/Machines/Machines.lua
+++ prototypes/Machines/Machines.lua
@@ -9,13 +9,13 @@ data.raw["offshore-pump"]["offshore-pump"].fluid = "Water_dirty"
 
 data:extend(
 {
-	{
+{
     type = "assembling-machine",
     name = "Crusher",
-    icon = "__base__/graphics/icons/assembling-machine-1.png",
+    icon = "__base__/graphics/icons/assembling-machine-2.png",
     flags = {"placeable-neutral", "placeable-player", "player-creation"},
     minable = {hardness = 0.2, mining_time = 0.5, result = "Crusher"},
-    max_health = 200,
+    max_health = 250,
     corpse = "big-remnants",
     dying_explosion = "medium-explosion",
     resistances =
@@ -25,29 +25,55 @@ data:extend(
         percent = 70
       }
     },
+    fluid_boxes =
+    {
+      {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {1, -2} }}
+      },
+	  {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {-1, -2} }}
+      },
+      {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {1, 2} }}
+      },
+	  {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {-1, 2} }}
+      },
+      off_when_no_fluid_recipe = true
+    },
     collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
     selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-    fast_replaceable_group = "assembling-machine",
+    fast_replaceable_group = "Crusher",
     animation =
     {
-      filename = "__base__/graphics/entity/assembling-machine-1/assembling-machine-1.png",
-      priority="high",
-      width = 99,
-      height = 102,
+      filename = "__base__/graphics/entity/assembling-machine-2/assembling-machine-2.png",
+      priority = "high",
+      width = 113,
+      height = 99,
       frame_count = 32,
       line_length = 8,
-      shift = {0.25, -0.1}
+      shift = {0.4, -0.06}
     },
-    crafting_categories = {"Crusher"},
-    crafting_speed = 1,
-    energy_source =
-    {
-      type = "electric",
-      usage_priority = "secondary-input",
-      emissions = 0.05 / 1.5
-    },
-    energy_usage = "90kW",
-    ingredient_count = 2,
     open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
     close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
     vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
@@ -55,17 +81,32 @@ data:extend(
     {
       sound = {
         {
-          filename = "__base__/sound/assembling-machine-t1-1.ogg",
+          filename = "__base__/sound/assembling-machine-t2-1.ogg",
           volume = 0.8
         },
         {
-          filename = "__base__/sound/assembling-machine-t1-2.ogg",
+          filename = "__base__/sound/assembling-machine-t2-2.ogg",
           volume = 0.8
         },
       },
       idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
       apparent_volume = 1.5,
-    }
+    },
+    crafting_categories = {"Crusher"},
+    crafting_speed = 0.75,
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      emissions = 0.04 / 2.5
+    },
+    energy_usage = "150kW",
+    ingredient_count = 4,
+    module_specification =
+    {
+      module_slots = 2
+    },
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"}
   },
   {
     type = "assembling-machine",
@@ -3105,6 +3146,105 @@ data:extend(
       apparent_volume = 1.5,
     },
     crafting_categories = {"Refinery"},
+    crafting_speed = 0.75,
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      emissions = 0.04 / 2.5
+    },
+    energy_usage = "150kW",
+    ingredient_count = 4,
+    module_specification =
+    {
+      module_slots = 2
+    },
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"}
+  },
+  {
+    type = "assembling-machine",
+    name = "Electric_flash_furnace",
+    icon = "__base__/graphics/icons/assembling-machine-2.png",
+    flags = {"placeable-neutral", "placeable-player", "player-creation"},
+    minable = {hardness = 0.2, mining_time = 0.5, result = "Electric_flash_furnace"},
+    max_health = 250,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 70
+      }
+    },
+    fluid_boxes =
+    {
+      {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {1, -2} }}
+      },
+	  {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {-1, -2} }}
+      },
+      {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {1, 2} }}
+      },
+	  {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {-1, 2} }}
+      },
+      off_when_no_fluid_recipe = true
+    },
+    collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
+    selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+    fast_replaceable_group = "Electric_flash_furnace",
+    animation =
+    {
+      filename = "__base__/graphics/entity/assembling-machine-2/assembling-machine-2.png",
+      priority = "high",
+      width = 113,
+      height = 99,
+      frame_count = 32,
+      line_length = 8,
+      shift = {0.4, -0.06}
+    },
+    open_sound = { filename = "__base__/sound/machine-open.ogg", volume = 0.85 },
+    close_sound = { filename = "__base__/sound/machine-close.ogg", volume = 0.75 },
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    working_sound =
+    {
+      sound = {
+        {
+          filename = "__base__/sound/assembling-machine-t2-1.ogg",
+          volume = 0.8
+        },
+        {
+          filename = "__base__/sound/assembling-machine-t2-2.ogg",
+          volume = 0.8
+        },
+      },
+      idle_sound = { filename = "__base__/sound/idle1.ogg", volume = 0.6 },
+      apparent_volume = 1.5,
+    },
+    crafting_categories = {"Electric_flash_furnace"},
     crafting_speed = 0.75,
     energy_source =
     {
