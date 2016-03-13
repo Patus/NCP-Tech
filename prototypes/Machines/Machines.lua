590d4ae3f43a9fef723e241403c3c6e9b4b05353
Patus <patsi91@hotmail.com>
2016-03-13 19:44:28 +0200

    machine fix

--- prototypes/Machines/Machines.lua
+++ prototypes/Machines/Machines.lua
@@ -1317,6 +1317,22 @@ data:extend(
         base_level = -1,
         pipe_connections = {{ type="input", position = {-1, -2} }}
       },
+	  {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {2, -1} }}
+      },
+	  {
+        production_type = "input",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = -1,
+        pipe_connections = {{ type="input", position = {-2, -1} }}
+      },
       {
         production_type = "output",
         pipe_picture = assembler2pipepictures(),
@@ -1332,6 +1348,22 @@ data:extend(
         base_area = 10,
         base_level = 1,
         pipe_connections = {{ type="output", position = {-1, 2} }}
+      },
+	  {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {2, 1} }}
+      },
+	  {
+        production_type = "output",
+        pipe_picture = assembler2pipepictures(),
+        pipe_covers = pipecoverspictures(),
+        base_area = 10,
+        base_level = 1,
+        pipe_connections = {{ type="output", position = {-2, 1} }}
       },
       off_when_no_fluid_recipe = true
     },
