a229ecdcf82bb6e552a8e93d7359d1b85da6a2c0
Patus <patsi91@hotmail.com>
2016-04-16 23:14:56 +0300

    fix/add underground pipes

--- prototypes/recipe/Recipes_2.lua
+++ prototypes/recipe/Recipes_2.lua
@@ -167,38 +167,74 @@ data:extend(
 }
 )
 
-local items={"gear","plate","rod","foil","pipe","chain","transmission_chain","wire"}
+local items={"gear","plate","rod","foil","pipe","chain","transmission_chain","wire","underground_pipe"}
 local materials={"Iron","Copper","Steel","Tin","Gold","Nickel","Zirconium","Aluminium","Lead","Titanium","Silver","Stainless_steel","Plastic","Magnesium","Tungsten","Brass","Nichrome"}
-local billetName={["Iron"]="Iron_billet",["Copper"]="Copper_billet",["Steel"]="Steel_billet",["Tin"]="Tin_billet",["Gold"]="Gold_ingot",["Nickel"]="Nickel_billet",["Zirconium"]="Zirconium_billet",["Aluminium"]="Aluminium_billet",["Lead"]="Lead_billet",["Titanium"]="Titanium_billet",["Silver"]="Silver_ingot",["Stainless_steel"]="Stainless_steel_billet",["Plastic"]="Plastic_pellets",["Magnesium"]="Magnesium_billet",["Tungsten"]="Tungsten_carbide_billet",["Concrete"]="Concrete_block",["Brass"]="Brass_billet",["Nichrome"]="Nichrome"}
-local itemMachines={["gear"]="",["plate"]="Rolling_machine",["rod"]="Extruder",["foil"]="Rolling_machine",["pipe"]="",["chain"]="",["transmission_chain"]="",["wire"]="Extruder"}
-local itemOutputs={["gear"]=1,["plate"]=2,["rod"]=4,["foil"]=4,["pipe"]=1,["chain"]=1,["transmission_chain"]=1,["wire"]=2}
+local billetName={["Iron"]="Iron_billet",["Copper"]="Copper_billet",["Steel"]="Steel_billet",["Tin"]="Tin_billet",["Gold"]="Gold_ingot",["Nickel"]="Nickel_billet",["Zirconium"]="Zirconium_billet",
+					["Aluminium"]="Aluminium_billet",["Lead"]="Lead_billet",["Titanium"]="Titanium_billet",["Silver"]="Silver_ingot",["Stainless_steel"]="Stainless_steel_billet",
+					["Plastic"]="Plastic_pellets",["Magnesium"]="Magnesium_billet",["Tungsten"]="Tungsten_carbide_billet",["Concrete"]="Concrete_block",["Brass"]="Brass_billet",
+					["Nichrome"]="Nichrome"}
+					
+local itemMachines={["gear"]=nil,["plate"]="Rolling_machine",["rod"]="Extruder",["foil"]="Rolling_machine",["pipe"]=nil,["chain"]=nil,
+					["transmission_chain"]=nil,["wire"]="Extruder",["underground_pipe"]=nil}
+local itemOutputs={["gear"]=1,["plate"]=2,["rod"]=4,["foil"]=4,["pipe"]=1,["chain"]=1,["transmission_chain"]=1,["wire"]=2,["underground_pipe"]=1}
 local itemInputs={["gear"]=1,["plate"]=1,["rod"]=1,["foil"]=1,["pipe"]=2,["chain"]=1,["transmission_chain"]=2,["wire"]=1}
 local inputItem={["gear"]="plate",["foil"]="plate",["pipe"]="plate",["chain"]="rod",["wire"]="rod"}
+
+
+
+local undergroundpipeLenght={["Iron"]=10,["Copper"]=10,["Steel"]=10,["Tin"]=10,["Gold"]=10,["Nickel"]=10,
+					["Aluminium"]=10,["Lead"]=10,["Titanium"]=10,["Silver"]=10,["Stainless_steel"]=10,
+					["Plastic"]=10,["Magnesium"]=10,["Tungsten"]=10,["Brass"]=10}
+					
+local pipeSize={["Iron"]=1,["Copper"]=1,["Steel"]=1,["Tin"]=1,["Gold"]=1,["Nickel"]=1,
+					["Aluminium"]=1,["Lead"]=1,["Titanium"]=1,["Silver"]=1,["Stainless_steel"]=1,
+					["Plastic"]=1,["Magnesium"]=1,["Tungsten"]=1,["Brass"]=1}
+
 
 
 
 
 function makeItems(material,item)
 	local itemName=material.."_"..item
-	
-	data:extend(
-    {
-      {
-		type= "item",
-		name= itemName,
-		icon = "__NCP-Tech__/graphics/icons/"..itemName..".png",
-		flags= { "goes-to-main-inventory" },
-		subgroup = item,
-		order= "a-b-c",
-		stack_size= 50,
-	},
+	if(item=="pipe" or item=="underground_pipe")then
+		
+		data:extend(
+		{
+		{
+			type= "item",
+			name= itemName,
+			icon = "__NCP-Tech__/graphics/icons/"..itemName..".png",
+			flags= { "goes-to-main-inventory" },
+			subgroup = item,
+			order= "a-b-c",
+			stack_size= 50,
+			place_result =itemName ,
+		},
 
-	})
+		})
+		makeEntities(material,item)
+	else
+		data:extend(
+		{
+		{
+			type= "item",
+			name= itemName,
+			icon = "__NCP-Tech__/graphics/icons/"..itemName..".png",
+			flags= { "goes-to-main-inventory" },
+			subgroup = item,
+			order= "a-b-c",
+			stack_size= 50,
+		},
+
+		})
+	end
+	
 end
 
 function makeRecipes(material,item)
 local itemName=material.."_"..item
-	if(item=="transmission_chain")then
+	if(item=="transmission_chain" or item=="underground_pipe")then
+		if(item=="transmission_chain")then
 		data:extend(
 		{
 		{
@@ -220,6 +256,29 @@ local itemName=material.."_"..item
 		subgroup = item,
 		},
 		})
+		end
+		if(item=="underground_pipe")then
+			data:extend(
+		{
+		{
+		type = "recipe",
+		name = itemName,
+		category =itemMachines[item],
+		energy_required = 2,
+		enabled = "true",
+		ingredients =
+		{
+			{type="item", name=material.."_pipe", amount=undergroundpipeLenght[material]+4},
+		},
+		results = 
+		{
+			{type="item", name=itemName, amount=itemOutputs[item]},
+		},
+		icon = "__NCP-Tech__/graphics/icons/"..itemName..".png",
+		subgroup = item,
+		},
+		})
+		end
 	else
 		if(item=="plate" or item=="rod")then
 			data:extend(
@@ -276,7 +335,140 @@ local itemName=material.."_"..item
 
 end
 
-
+function makeEntities(material,item)
+local itemName=material.."_"..item
+if(item=="pipe")then
+	data:extend(
+	{
+		{
+		type = "pipe",
+		name = itemName,
+		icon ="__NCP-Tech__/graphics/icons/"..itemName..".png",
+		flags = {"placeable-neutral", "player-creation"},
+		minable = {hardness = 0.2, mining_time = 0.5, result = itemName},
+		max_health = 50,
+		corpse = "small-remnants",
+		resistances =
+		{
+		{
+			type = "fire",
+			percent = 90
+		}
+		},
+		fast_replaceable_group = "pipe",
+		collision_box = {{-0.29, -0.29}, {0.29, 0.29}},
+		selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+		fluid_box =
+		{
+		base_area = pipeSize[material],
+		pipe_connections =
+		{
+			{ position = {0, -1} },
+			{ position = {1, 0} },
+			{ position = {0, 1} },
+			{ position = {-1, 0} }
+		},
+		},
+		pictures = pipepictures(),
+		working_sound =
+		{
+		sound = {
+			{
+			filename = "__base__/sound/pipe.ogg",
+			volume = 0.85
+			},
+		},
+		match_volume_to_activity = true,
+		max_sounds_per_type = 3
+		},
+		horizontal_window_bounding_box = {{-0.25, -0.25}, {0.25, 0.15625}},
+		vertical_window_bounding_box = {{-0.28125, -0.40625}, {0.03125, 0.125}}
+	},
+	
+	})
+	end
+	if(item=="underground_pipe")then
+		data:extend(
+		{
+			{
+			type = "pipe-to-ground",
+			name = itemName,
+			icon = "__NCP-Tech__/graphics/icons/"..itemName..".png",
+			flags = {"placeable-neutral", "player-creation"},
+			minable = {hardness = 0.2, mining_time = 0.5, result = itemName},
+			max_health = 50,
+			corpse = "small-remnants",
+			resistances =
+			{
+			{
+				type = "fire",
+				percent = 80
+			}
+			},
+			collision_box = {{-0.29, -0.29}, {0.29, 0.2}},
+			selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
+			fluid_box =
+			{
+			base_area = pipeSize[material],
+			pipe_covers = pipecoverspictures(),
+			pipe_connections =
+			{
+				{ position = {0, -1} },
+				{
+				position = {0, 1},
+				max_underground_distance = undergroundpipeLenght[material]
+				}
+			},
+			},
+			underground_sprite =
+			{
+			filename = "__core__/graphics/arrows/underground-lines.png",
+			priority = "high",
+			width = 32,
+			height = 32
+			},
+			vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+			pictures =
+			{
+			up =
+			{
+				filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-up.png",
+				priority = "high",
+				width = 44,
+				height = 32 --, shift = {0.10, -0.04}
+			},
+			down =
+			{
+				filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-down.png",
+				priority = "high",
+				width = 40,
+				height = 32 --, shift = {0.05, 0}
+			},
+			left =
+			{
+				filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-left.png",
+				priority = "high",
+				width = 32,
+				height = 42 --, shift = {-0.12, 0.1}
+			},
+			right =
+			{
+				filename = "__base__/graphics/entity/pipe-to-ground/pipe-to-ground-right.png",
+				priority = "high",
+				width = 32,
+				height = 40 --, shift = {0.1, 0.1}
+			},
+			}
+			},
+		
+		
+		
+		})
+		
+		
+		
+	end
+end
 
 
 for i , item in pairs(items) do
@@ -284,7 +476,7 @@ for i , item in pairs(items) do
 		if(not(material=="Zirconium" or material=="Nichrome")) then
 			makeItems(material,item)
 			makeRecipes(material,item)
-
+			
 		else
 			if((material=="Zirconium" and item=="rod")or(material=="Nichrome" and (item=="rod"or item=="wire")))then
 				makeItems(material,item)
