f7cc542421547abfa76ea2eb2930a08223aa5ac5
Corwent <johan.peltonen@hotmail.com>
2016-04-17 16:15:21 +0300

    fuu

---
+++ prototypes/items/ammo.lua
@@ -0,0 +1,40 @@
+data:extend(
+  {
+    type = "ammo",
+    name = "lead_shot",
+    icon = "__NCP-Tech__/graphics/icons/Lead_shot.png",
+    flags = {"goes-to-main-inventory"},
+    ammo_type =
+    {
+      category = "lead-shot",
+      action =
+      {
+        type = "direct",
+        action_delivery =
+        {
+          type = "instant",
+          source_effects =
+          {
+              type = "create-explosion",
+              entity_name = "explosion-gunshot"
+          },
+          target_effects =
+          {
+            {
+              type = "create-entity",
+              entity_name = "explosion-hit"
+            },
+            {
+              type = "damage",
+              damage = { amount = 5 , type = "physical"}
+            }
+          }
+        }
+      }
+    },
+    magazine_size = 1,
+    subgroup = "ammo",
+    order = "a-b-c",
+    stack_size = 200
+  }
+  )
---
+++ prototypes/recipe/ammo.lua
@@ -0,0 +1,14 @@
+data:extend(
+{
+    type = "recipe",
+    name = "lead-shot",
+    enabled = true,
+    energy_required = 3,
+    ingredients =
+    {
+      {"lead_rod", 1},
+      {"gunpowder", 1}
+    },
+    result = "lead_shot"
+  }
+  )
