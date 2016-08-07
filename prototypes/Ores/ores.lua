0e6267001d2e1658c569d72e17860817d96bb1ba
Patus <patsi91@hotmail.com>
2016-08-07 21:51:08 +0300

    ore balancing

--- prototypes/Ores/ores.lua
+++ prototypes/Ores/ores.lua
@@ -22,22 +22,18 @@ data.raw.resource["stone"].autoplace = (
   {
     control = "stone",
     sharpness = 1,
-    richness_multiplier = 15000,
-    richness_base = 350,
+    richness_multiplier = 1500,
+    richness_base = 500,
+	richness_multiplier_distance_bonus = 20,
     size_control_multiplier = 0.06,
+	coverage = 0.02,
+	starting_area_amount = 1500,
     peaks =
     {
       {
-        influence = 0.2,
-      },
-      {
-        influence = 0.65,
         noise_layer = "stone",
-        noise_octaves_difference = -1.9,
-        noise_persistence = 0.35,
-        starting_area_weight_optimal = 0,
-        starting_area_weight_range = 0,
-        starting_area_weight_max_range = 2,
+        noise_octaves_difference = -1.5,
+		noise_persistence = 0.3,
       },
       
     }
@@ -79,32 +75,191 @@ data.raw.resource["crude-oil"].minable = (
 )
 
 
+function makeore (name,results)
+data:extend(
+{
+ {
+    type = "resource",
+    name = name,
+    icon = "__NCP-Tech__/graphics/item/"..name..".png",
+    flags = {"placeable-neutral"},
+    order="a-b-a",
+    minable =
+    {
+      hardness = 0.9,
+      mining_particle = "copper-ore-particle",
+      mining_time = 2,
+      results=results,
+    },
+    collision_box = {{ -0.1, -0.1}, {0.1, 0.1}},
+    selection_box = {{ -0.5, -0.5}, {0.5, 0.5}},
+    autoplace =
+    {
+      control = name,
+      sharpness = 1,
+      richness_multiplier = 700,
+      richness_base = 400,
+	  richness_multiplier_distance_bonus = 20,
+      size_control_multiplier = 0.2,
+	  coverage = 0.04,
+	  starting_area_amount = 500,
+	  random_probability_penalty=0,
+	  --max_probability = 0.035,
+      peaks = {
+
+        {
+			noise_layer = name,
+			noise_octaves_difference = -0.19,
+			noise_persistence = 0.60,
+        },
+      },
+    },
+    stage_counts = {5000, 4000, 3000, 2000, 1000, 500, 100, 1},
+    stages =
+    {
+      sheet =
+      {
+        filename = "__NCP-Tech__/graphics/entity/Ores/"..name..".png",
+        priority = "extra-high",
+        width = 38,
+        height = 38,
+        frame_count = 4,
+        variation_count = 8
+      }
+    },
+    map_color = {r=math.random(), g=math.random(), b=math.random()}
+  }
+  })
+
+if name=="Clay_ore" then
+data.raw.resource[name].icon="__NCP-Tech__/graphics/item/Clay_wet.png"
+end
+
+end
+
+
+makeore ("Graphite_ore",{		{type="item", name="Graphite_ore", amount=1, probability = 1},
+								{type="item", name="Diamond_uncut", amount=1, probability = 0.001},
+})
+----[[
+makeore ("Clay_ore",{			{type="item", name="Clay_wet", amount=1, probability = 1}
+})
+makeore ("Copper_native_ore",{	{type="item", name="Copper_native_ore", amount=1, probability = 1}
+})
+makeore ("Scheelite_ore",{		{type="item", name="Scheelite_ore", amount=1, probability = 1}
+})
+makeore ("Chalcopyrite_ore",{	{type="item", name="Chalcopyrite_ore", amount=1, probability = 1},
+								{type="item", name="Pentlandite_ore", amount=1, probability = 0.25},
+								{type="item", name="Sulfur", amount=1, probability = 0.10}
+								})
+makeore ("Uraninite_ore",{		{type="item", name="Uraninite_ore", amount=1, probability = 1}
+})
+makeore ("Fluorite_ore",{		{type="item", name="Fluorite_ore", amount=1, probability = 1}
+})
+makeore ("Cassiterite_ore",{	{type="item", name="Cassiterite_ore", amount=1, probability = 1}
+})
+makeore ("Galena_ore",{			{type="item", name="Galena_ore", amount=1, probability = 1},
+								{type="item", name="Sphalerite_ore", amount=1, probability = 0.25},
+								{type="item", name="Sulfur", amount=1, probability = 0.10}
+})
+makeore ("Rutile_ore",{			{type="item", name="Rutile_ore", amount=1, probability = 1}
+})
+makeore ("Magnetite_ore",{		{type="item", name="Magnetite_ore", amount=1, probability = 1},
+								{type="item", name="Chromite_ore", amount=1, probability = 0.10}
+})
+makeore ("Bauxite_ore",{{type="item", name="Bauxite_ore", amount=1, probability = 1}
+})
+makeore ("Bauxite_ore",{{type="item", name="Bauxite_ore", amount=1, probability = 1}
+})
+makeore ("Bauxite_ore",{{type="item", name="Bauxite_ore", amount=1, probability = 1}
+})
+makeore ("Bauxite_ore",{{type="item", name="Bauxite_ore", amount=1, probability = 1}
+})
+
+
+
+--]]
+
+
 
 
 data:extend(
 {
- 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  {
+    type = "resource",
+    name = "Brine_pool",
+    icon = "__NCP-Tech__/graphics/fluid/Brine.png",
+    flags = {"placeable-neutral"},
+    category = "basic-fluid",
+    order="a-b-a",
+    infinite = true,
+    minimum = 1500,
+    normal = 15000,
+    minable =
+    {
+      hardness = 1,
+      mining_time = 1,
+      results =
+      {
+        {
+          type = "fluid",
+          name = "Brine",
+          amount_min = 1,
+          amount_max = 1,
+          probability = 1,
+        }
+      }
+    },
+    collision_box = {{ -1.4, -1.4}, {1.4, 1.4}},
+    selection_box = {{ -0.5, -0.5}, {0.5, 0.5}},
+    autoplace =
+    {
+      control = "Brine_pool",
+      sharpness = 0.99,
+      max_probability = 0.035,
+      richness_base = 6000,
+      richness_multiplier = 30000,
+      richness_multiplier_distance_bonus = 10,
+      coverage = 0.02, -- Cover on average 2% of surface area.
+      peaks =
+      {
+        {
+          noise_layer = "Brine_pool",
+          noise_octaves_difference = -1,
+          noise_persistence = 0.4,
+        }
+      }
+    },
+    stage_counts = {0},
+    stages =
+    {
+      sheet =
+      {
+        filename = "__NCP-Tech__/graphics/entity/Ores/Brine_ore.png",
+        priority = "extra-high",
+        width = 75,
+        height = 61,
+        frame_count = 4,
+        variation_count = 1
+      }
+    },
+    map_color = {r=0.3, g=0.3, b=1},
+    map_grid = false
+  }
+})	  
+
+
+
+
+
+
+
+
+
+
+--[[
+data:extend(
+{
   
   {
     type = "resource",
@@ -129,25 +284,19 @@ data:extend(
     {
       control = "Graphite_ore",
       sharpness = 1,
-      richness_multiplier = 13000,
-      richness_base = 350,
+      richness_multiplier = 1500,
+      richness_base = 500,
+	  richness_multiplier_distance_bonus = 20,
       size_control_multiplier = 0.06,
+	  coverage = 0.02,
+	  starting_area_amount = 1500,
+	  
       peaks = {
+
         {
-          influence = 0.2,
-          starting_area_weight_optimal = 0,
-          starting_area_weight_range = 0,
-          starting_area_weight_max_range = 2,
-        },
-        {
-          influence = 0.65,
-          
-          noise_octaves_difference = -1.9,
-		  noise_layer = "Graphite_ore",
-          noise_persistence = 0.3,
-          starting_area_weight_optimal = 0,
-          starting_area_weight_range = 0,
-          starting_area_weight_max_range = 2,
+			noise_layer = "Graphite_ore",
+			noise_octaves_difference = -1.5,
+			noise_persistence = 0.3,
         },
       },
     },
@@ -870,3 +1019,4 @@ data:extend(
   },
 }
 )
+--]]
